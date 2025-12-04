ab67acf2006a2b31c844014221736d10578b9918
Serafet <serafet6767@gmail.com>
2025-12-04 17:39:32 +0300

    fix

--- middlewares/checkRole.middleware.ts
+++ middlewares/checkRole.middleware.ts
@@ -12,10 +12,7 @@ export const checkRole = (...allowedRoles: USER_ROLE[]) => {
     const userRole = req.user.role;
 
     if (!allowedRoles.includes(userRole)) {
-      throw new AppError(
-        "Forbidden: You are not allowed to access this resource",
-        403
-      );
+      throw new AppError("Forbidden: Insufficient permissions", 403);
     }
 
     next();
